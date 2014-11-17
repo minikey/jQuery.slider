a9c502efac7fb498a5eedf7b9a68dd0d7ef3f337
FMin <635548528@qq.com>
2014-11-18 01:28:20 +0800

    Update jquery.slider.js

--- jquery.slider.js
+++ jquery.slider.js
@@ -5,9 +5,9 @@
 		animduration: 200, // 动画时间
 		animspeed: 3000, // 延迟时间
 		animtype: 'push', // 动画效果
-		direction: 'left', // 默认移动方向
 		automatic: true, // 自动开始滚动
 		curindex: 0, // 当前下标
+		fixspace: 10,
 		itemparent: '', // 显示项的父节点 可以是标签名、类名、id
 		items:  '' // 显示项，配置传入egg： 'ul.news li'
 	};
@@ -15,17 +15,42 @@
 	var animate = {
 		push: {
 			init: function() {
-				var data = this.data('_data');
-				console.log(data);
+				var data = this.data('_data'),
+					option = this.data('option'),
+					width = data.width,
+					size = data.items.length;
+				data.itemparent.width(width * size + option.fixspace);
 			},
 			prev: function() {
-
+				var option = this.data('option'),
+					data = this.data('_data'),
+					len = data.items.length;
+				show((len - 1 + option.curindex) % len);
 			},
 			next: function() {
-
+				var option = this.data('option'),
+					data = this.data('_data'),
+					len = data.items.length;
+				show((len + 1 + option.curindex) % len);
 			},
-			show: function() {
+			show: function(index) {
+				var option = this.data('option'),
+					data = this.data('_data'),
+					cur = option.curindex,
+					det = -index * data.width,
+					p = data.itemparent,
+					dir = option.direction,
+					that = this;
+				if (cur == index) {
+					return;
+				}
 
+				this.trigger('beforeAnimate', [cur]);
+
+				p.stop(false, true).animate({left: det + 'px'}, option.animduration, function() {
+					option.curindex = index;
+					that.trigger('afterAnimate', [index]);
+				});
 			}
 		}
 	};
@@ -48,21 +73,26 @@
 		show: function(index) {
 			var dom = this.dom,
 				option = dom.data('option');
-			animate[option.animtype].show.call(dom);
+			animate[option.animtype].show.call(dom, index);
 		}
 	};
 
 	function next() {
-		$(this).data();
+		$(this).data('controller').next();
 	}
 
 	function prev() {
+		$(this).data('controller').prev();
+	}
 
+	function show(index) {
+		$(this).data('controller').show(index);
 	}
 
 	function bindEvents() {
 		this.on('next', next)
-			.on('prev', prev);
+			.on('prev', prev)
+			.on('show', show);
 	}
 
 	function initAnimation(name) {
@@ -71,7 +101,7 @@
 	}
 
 	function init(opt) {
-		var me = $(this), data = {};
+		var me = this, data = {};
 		
 		if(!me[0] || !opt) {
 			return;
@@ -83,17 +113,18 @@
 
 		if(data.items.length) {
 			data.width = data.items.eq(0).outerWidth();
-			initAnimation.call(me, opt.animtype);
 			data.itemparent = data.items.eq(0).closest(opt.itemparent);
-		}
-
-		this.data('controller', new Controller(me))
+			this.data('controller', new Controller(this))
 			.data('option', opt)
 			.data('_data', data);
+			initAnimation.call(this, opt.animtype);
+		}
+
+		
 	}
 
 	init.defaults = defaults;
 	init.animate = animate;
 
-	$.slider = init;
+	$.fn.slider = init;
 })(jQuery);
